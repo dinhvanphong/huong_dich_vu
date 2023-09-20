fc6d2d57ff072d7551f4de2681cb74d103f033ff
dinhvanphong <dinhvanphongv4@gmail.com>
2023-09-20 20:58:41 +0700

    fsfs

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,10 +1,20 @@
-
+const Course = require('../models/Course');
+const { mutipleMongooseToObject } = require('../../util/mongoose');
 
 class SiteController{
     // [GET] /
     index(req, res){
-        res.render('home')
+        Course.find({}) 
+        .then((courses) => {
+            res.render('home', {
+                courses: mutipleMongooseToObject(courses),
+            });
+        })
+        .catch(err => next(err))
     }
+    // index(req, res){
+    //     res.render('home')
+    // }
 
     search(req, res){
         res.render('search')
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const newRouter = require('./new')
 const siteRouter = require('./site')
+const coursesRouter = require('./courses')
 
 function routes(app){
 
       app.use('/new', newRouter)
+      app.use('/courses', coursesRouter)
       app.use('/', siteRouter)
       
      
